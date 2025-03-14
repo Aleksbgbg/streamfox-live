c238cbb28471283529b5e9218e7ff3246dced34b
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-03-14 22:08:08 +0000

    backend: Implement HTTP server code
    Create a HTTP server via axum, allowing us to respond to requests as we
    build up the frontend.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,77 @@
+use axum::Router;
+use std::net::SocketAddr;
+use thiserror::Error;
+use tokio::net::TcpListener;
+use tokio::{select, signal};
+use tracing::{Level, error, info};
+
+#[derive(Debug, Error)]
+enum AppError {
+  #[error("could not bind to network interface: {0}")]
+  BindTcpListener(std::io::Error),
+  #[error("could not get TCP listener address: {0}")]
+  GetListenerAddress(std::io::Error),
+  #[error("could not start Axum server: {0}")]
+  ServeApp(std::io::Error),
+}
+
+#[tokio::main]
+async fn start() -> Result<(), AppError> {
+  let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 8001)))
+    .await
+    .map_err(AppError::BindTcpListener)?;
+
+  let app = Router::new();
+
+  info!(
+    "backend listening on {}",
+    listener
+      .local_addr()
+      .map_err(AppError::GetListenerAddress)?
+  );
+
+  axum::serve(listener, app)
+    .with_graceful_shutdown(shutdown_signal())
+    .await
+    .map_err(AppError::ServeApp)?;
+
+  Ok(())
+}
+
 fn main() {
-  println!("Hello, world!");
+  tracing_subscriber::fmt()
+    .with_target(false)
+    .compact()
+    .with_max_level(Level::DEBUG)
+    .init();
+
+  info!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
+  match start() {
+    Ok(_) => info!("app exited successfully"),
+    Err(err) => error!("app exited due to error: {}", err),
+  }
+}
+
+async fn shutdown_signal() {
+  let ctrl_c = async {
+    signal::ctrl_c()
+      .await
+      .expect("failed to install Ctrl+C handler");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+    signal::unix::signal(signal::unix::SignalKind::terminate())
+      .expect("failed to install SIGTERM handler")
+      .recv()
+      .await;
+  };
+  #[cfg(not(unix))]
+  let terminate = std::future::pending::<()>();
+
+  select! {
+    _ = ctrl_c => {},
+    _ = terminate => {},
+  }
 }
