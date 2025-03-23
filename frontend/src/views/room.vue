952974fc447501019a87f44b814a47345e562da4
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-03-23 12:04:58 +0000

    frontend: Reject navigation to invalid room names
    If the user attempts to enter the room page with an invalid name,
    redirect back to the main page.

--- frontend/src/views/room.vue
+++ frontend/src/views/room.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-defineProps<{
+import { useFetch } from "@/api";
+import { router } from "@/router";
+
+const props = defineProps<{
   name: string;
 }>();
+
+const { error } = await useFetch({
+  method: "post",
+  url: "/room/validate-name",
+  data: { name: props.name },
+  immediate: true,
+});
+
+if (error.value) {
+  router.push({ name: "main" });
+}
 </script>
 
 <template>
