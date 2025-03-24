6b51cdecbb0935150fe792c97f72547ef30bd2e4
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-03-24 19:07:10 +0000

    frontend: Create room page layout
    Create the base layout for the room page, consisting of a title at the
    top, a video stream region, and a series of buttons at the bottom.
    
    Only the user button was added in this change, which will be used to
    count the number of connected users, and later on to display their names
    when clicked.
    
    Some app-level changes were made to accomodate this new layout:
     - to achieve a full screen view, `h-screen` was added to the app div
     - a depencency on `@heroicons/vue` was added for button icons

--- frontend/src/views/room.vue
+++ frontend/src/views/room.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { UserIcon } from "@heroicons/vue/24/solid";
 import { useFetch } from "@/api";
 import { router } from "@/router";
 
@@ -19,5 +20,22 @@ if (error.value) {
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
+  <div class="flex h-full flex-col py-3">
+    <div>
+      <h1 class="text-center text-2xl font-bold">{{ name }}</h1>
+    </div>
+    <div class="flex grow flex-col">
+      <p class="my-auto text-center text-xl">no active stream</p>
+    </div>
+    <div class="flex">
+      <div class="mx-auto">
+        <div class="indicator">
+          <span class="indicator-item badge badge-secondary">0</span>
+          <button class="btn btn-neutral btn-square">
+            <UserIcon class="size-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
