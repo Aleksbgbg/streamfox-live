a6c1f86683db0994a511877b89a940ede5331659
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-04-06 23:09:25 +0100

    frontend: api: Switch post and put
    Just a personal preference for ordering creation before updating.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -11,7 +11,7 @@ type KVMap = Record<string, unknown>;
 const instance = axios.create();
 
 export async function useFetch<Data, Error = GenericError>(params: {
-  method: "get" | "put" | "post" | "delete";
+  method: "get" | "post" | "put" | "delete";
   url: string;
   data?: KVMap;
   immediate: boolean;
